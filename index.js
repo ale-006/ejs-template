b64dadf1f318964edb5442d6d710e580bb023aa0
Alessandro Baio <77743955+ale-006@users.noreply.github.com>
2022-03-02 15:48:17 +0100

    v 1.0

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const db = require("quick.db")
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+  res.render("index")
+})
+
+app.get("/example", (req, res) => {
+  res.render("example", {
+    name: "john"
+  })
+})
+
+app.get("*", (req, res) => {
+  res.status(404).send("Error: 404. This page does not exist.")
+})
+
+app.listen(3000, () => console.log("Website Ready!"))
